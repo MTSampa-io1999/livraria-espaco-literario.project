eb6ccdc1835b4d15ef9abcaabfc907a924d720c6
Willian Gabriel <148078844+WilllianGabriel@users.noreply.github.com>
2024-01-20 21:38:03 -0300

    Update Catalogo.cs

--- Catalogo.cs
+++ Catalogo.cs
@@ -1,14 +1,12 @@
 public class Catalogo{
-    public void DisplayBookDetails(string titulo, string autor, int paginas, string descricao, decimal preco)
+    public void Detalhes(string titulo, string autor, int paginas, string descricao, decimal preco)
         {
             Console.WriteLine($"{titulo} - {autor}");
             Console.WriteLine($"CAPA COMUM - {paginas} Páginas.");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine($"DESCRIÇÃO - {descricao}");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine($"R$ {preco}");
-            Console.WriteLine("");
+            Console.WriteLine();
         }
 }
-
-
